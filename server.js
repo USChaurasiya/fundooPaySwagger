2bb512c06c93ebde96f8cda76623dbf2242f9975
pranalipatil209 <pranalipatil209@gmail.com>
2017-03-14 15:30:23 +0530

    update swagger

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ var port = Number(process.env.PORT || 3000);
 var connect = require('connect');
 connect().use(connect.static(dirRoot)).listen(port);
 
-console.log('Static web server runnin on port: ' + port);
+console.log('Static web server running on port: ' + port);
